29790bdfe36eadcfcf55b2242ac2346d1294983f
carlos luna <clunag1800@alumno.ipn.mx>
2025-10-23 07:11:12 -0600

    introduccion

---
+++ Introduccion y variables/Introduccion.py
@@ -0,0 +1,29 @@
+# imprimir hola mundo en terminal
+print('Hola mundo\n')
+
+# Declaracion de variables en python
+nombre="carlos luna" #tipo string
+numero=89 #tipo entero
+flotante=3.1416 #tipo flotante
+
+#impresion tanto del tipo de variable y el valor de la variable
+
+print(type(nombre))
+print("Tu nombre es:", nombre,"\n")
+
+print(type(numero))
+print("tu numero es:", numero ,"\n")
+
+print(type(nombre))
+print("Tu numero es :", flotante, "\n")
+
+# declaracion de variables de tipo constantes
+PI=3.1416
+
+'''Asignacion 
+multiples 
+de variables '''
+
+nombre1, apellido1 ='Juan','luna'
+print(nombre1)
+print(apellido1)
