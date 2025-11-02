ec504a92e60a85e5e08c00e023edb773a3cb71b4
carlos luna <clunag1800@alumno.ipn.mx>
2025-11-01 22:58:52 -0600

    Boolean

---
+++ 5.booleanos/OperadoresComparacion.py
@@ -0,0 +1,15 @@
+num1=1000
+num2=500
+num3=1000
+
+cadena1="Comparaciones de Strings"
+cadena2="Comparacion de Strings"
+
+print(num2>num1)
+print(num2<num1)
+print(num1>= num3)
+print(num1<= num3)
+print(num1 == num3)
+print(num1 != num3)
+
+print(cadena1>cadena2)
---
+++ 5.booleanos/operadoresLogicos.py
@@ -0,0 +1,4 @@
+print(99!= 99 and 23==23)
+print(88<=45 or 60>110)
+print(not 90 != 90)
+print(not 100==100)
